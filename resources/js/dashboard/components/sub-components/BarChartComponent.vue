5ba7c2f12a46fae6f66ad07f8452238aa07c2adc
Ezekiel Reginio <ezekiel@1export.com>
2023-03-27 19:27:58 +0800

    installed vue chart js dependency

--- resources/js/dashboard/components/sub-components/BarChartComponent.vue
+++ resources/js/dashboard/components/sub-components/BarChartComponent.vue
@@ -1,21 +1,35 @@
 
 <template>
-    <charts-css
-        heading="Coffee count"
-        type="bar"
-        :labels="['Mon', 'Tue', 'Wed',]"
-        :datasets="[ { values: [4, 2, 70,] } ]"
+    <Bar
+        id="my-chart-id"
+        :options="chartOptions"
+        :data="chartData"
     />
 </template>
 
 <script>
-    
-    export default {
-        
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [ { data: [40, 20, 12] } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
     }
+  }
+}
 </script>
 
-<!-- <style scoped>
+<style scoped>
   
    
-</style> -->
+</style>
--- resources/js/dashboard/dashboard.js
+++ resources/js/dashboard/dashboard.js
@@ -1,12 +1,10 @@
 require('../bootstrap');
 
 import DashboardComponent from './components/DashboardComponent.vue'
-import VueChartsCSS from "vue.charts.css";
 
 import Vue from "vue";
 
 Vue.component('dashboard-component', DashboardComponent);
-Vue.use(VueChartsCSS);
 
 const app = new Vue({
     el      : '#app',
