2c631c9231613c190493386775471ad7eb605944
Ezekiel Reginio <ezekiel@1export.com>
2023-04-04 02:21:48 +0800

    removed unused methods in Account Group Controller

--- app/Http/Controllers/AccountGroupController.php
+++ app/Http/Controllers/AccountGroupController.php
@@ -19,26 +19,6 @@ class AccountGroupController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -61,17 +41,6 @@ class AccountGroupController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
     {
         //
     }
